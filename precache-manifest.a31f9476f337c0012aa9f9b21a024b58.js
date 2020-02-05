910b1c01d41ddf11f8e58b55a7c7f5e3a5a01459
VeronicaDem <veron.dem4enko2014@yandex.ru>
2020-02-05 12:07:46 +0300

    Updates

---
+++ precache-manifest.a31f9476f337c0012aa9f9b21a024b58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6500d62a1020ba410d09825d8f2b7340",
+    "url": "/century2/index.html"
+  },
+  {
+    "revision": "4a72fc71d86cd7fe4821",
+    "url": "/century2/static/js/2.600de584.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/century2/static/js/2.600de584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c0c3f7528cbb227a89",
+    "url": "/century2/static/js/main.28ddf3f1.chunk.js"
+  },
+  {
+    "revision": "969356c3c0b9c9228dd7",
+    "url": "/century2/static/js/runtime-main.779718f3.js"
+  }
+]);

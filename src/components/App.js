7f1de162380a38889130f449e5110e6e992ec091
Veronica <no.demchenko@yandex.ru>
2024-11-20 19:39:56 +0300

    refactor: formatting

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import About from './About';
-import { changeFields, hideForm, sendForm, showForm } from './actions';
-import Footer from './Footer';
-import Header from './Header';
-import Main from './Main';
-import Popup from './Popup';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import About from "./About";
+import { changeFields, hideForm, sendForm, showForm } from "./actions";
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Popup from "./Popup";
 function App(props) {
   return (
     <div className="App">
-     <Header {...props}/>
-     <Main {...props}/>
-     <Popup {...props}/>
-     <Footer />
-     <About />
+      <Header {...props} />
+      <Main {...props} />
+      <Popup {...props} />
+      <Footer />
+      <About />
     </div>
   );
 }
 
-export default connect((state)=>state,
-                      (dispatch)=>bindActionCreators({hideForm, changeFields,showForm, sendForm},dispatch))(App);
+export default connect(
+  (state) => state,
+  (dispatch) =>
+    bindActionCreators({ hideForm, changeFields, showForm, sendForm }, dispatch)
+)(App);

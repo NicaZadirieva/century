55a45785ffc5ec8ee7f1b100d30e30c924334fda
Veronica <no.demchenko@yandex.ru>
2024-11-24 10:24:09 +0300

    feat(BelievedUsCompanies): add styles

--- src/components/BelivedUsCompanies/index.tsx
+++ src/components/BelivedUsCompanies/index.tsx
@@ -1,17 +1,19 @@
+import cn from 'classnames';
+import Paragraph from '../Paragraph';
 import { BelievedUsCompaniesProps } from './BelievedUsCompanies.props';
-
+import styles from './index.module.css';
 export default function BelievedUsCompanies(props: BelievedUsCompaniesProps) {
     const companies = props.images;
     return (
         <>
-        <div className="companies d-flex flex-column">
-          <h2 className="company-title title">Нам доверяют</h2>
-          <div className="btn-more d-flex align-self-md-end align-self-sm-start flex-row ">
-            <div className="more-img companies-img"></div>
-            <p>Все клиенты</p>
+        <div className={cn(styles["companies"], "d-flex", "flex-column")}>
+          <h2 className={cn(styles["company-title"])}>Нам доверяют</h2>
+          <div className={cn("d-flex", "align-self-md-end", "align-self-sm-start", "flex-row")}>
+            <div className={cn(styles["more-img"], styles["companies-img"])}></div>
+            <Paragraph className='paragraph'>Все клиенты</Paragraph>
           </div>
         </div>
-        <div className="companies-block-img d-flex flex-row flex-wrap justify-content-around">
+        <div className={cn(styles["companies-block-img"], "d-flex", "flex-row", "flex-wrap", "justify-content-around")}>
           {
             companies.map((company, index) => (
                 <img alt={company.alt} key={index} src={company.url} width={company.width} height={company.height} />

7fbf7ec96d177b2ce017010bdb3f14989f752ea9
Veronica <no.demchenko@yandex.ru>
2024-12-09 12:25:33 +0300

    fix(CardSite): fix CardSite

--- src/components/CardSite/index.tsx
+++ src/components/CardSite/index.tsx
@@ -1,12 +1,17 @@
+import cn from 'classnames';
 import Paragraph from '../Paragraph';
 import { CardSiteProps } from './CardSite.props';
 import styles from './index.module.css';
 export default function CardSite(props: CardSiteProps) {
     return (
-        <div className="card-site position-relative">
+        <div className={cn(styles["card-site"], "position-relative")}>
             {/*TODO: Возможно этой блок лучше перевести на img*/}
-                <img className={styles['img-right']} src={props.imgRight.src} alt={props.imgRight.alt} />
-                <img className={styles['img-left']} src={props.imgLeft.src} alt={props.imgLeft.alt} />
+            <div className={styles['img-right']}>
+                <img className="img" src={props.imgRight.src} alt={props.imgRight.alt} />
+            </div>
+            <div className={styles['img-left']}>
+                <img className="img" src={props.imgLeft.src} alt={props.imgLeft.alt} />
+            </div>
             <Paragraph className={styles['paragraph']}>
               <a className={styles['link']} href="#">{props.title}</a>
             </Paragraph>

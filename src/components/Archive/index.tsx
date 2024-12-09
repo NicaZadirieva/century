36350741951b50df2ed9bc5a0577ce699e79dace
Veronica <no.demchenko@yandex.ru>
2024-12-09 12:53:47 +0300

    fix(Archive): now worked

--- src/components/Archive/index.tsx
+++ src/components/Archive/index.tsx
@@ -7,8 +7,8 @@ export default function Archive({children}: ArchiveProps) {
         <div className={cn(styles["main-container"], "d-flex", "flex-column")}>
           <h2 className={styles["title"]}>Архив новостей</h2>
           <div className={cn("d-flex", "align-self-md-end", "align-self-sm-start", "flex-row", "order-2", "order-md-1")}>
-            <div className={cn(styles["more-img"], styles["calendar-img"])}></div>
-            <p className={styles["paragraph"]}>Все новости</p>
+            <div className={styles["calendar-img"]}></div>
+            <p className={cn(styles["paragraph"], styles["upper-paragraph"])}>Все новости</p>
           </div>
           <div className={cn("order-1", "order-md-2", "d-flex", "flex-lg-row", "flex-column", "justify-content-between", "align-items-center")}>
             {children}

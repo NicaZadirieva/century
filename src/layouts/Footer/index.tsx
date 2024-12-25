b1699bfb83e8316fe3d422b9e4a273afe5b26477
Veronica <no.demchenko@yandex.ru>
2024-12-26 00:19:19 +0300

    refactor(Footer): simplify html + css

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -4,75 +4,59 @@ import styles from "./index.module.css";
 /**TODO: что-то в принципе можно выделить в константы. В том числе и компоненты */
 export default function Footer() {
   return (
-    <>
-      <div
-        className={cn(
-          styles["footer"],
-          "d-flex",
-          "flex-row",
-          "justify-content-between"
-        )}
-      >
-        <div className={cn("d-lg-flex", "flex-row", "desk")}>
-          <div className={styles["logo"]}></div>
-          <p className={styles["logo-title"]}>Новый век технологий</p>
+    <footer
+      className={cn(
+        styles["footer"],
+        "d-flex",
+        "flex-row",
+        "justify-content-between",
+        "align-items-center"
+      )}
+    >
+      <div className={cn(styles["logo"], "d-lg-flex", "flex-row", "align-items-center", "desk")}>
+        <div className={styles["logo__image"]}></div>
+        <p className={styles["logo__title"]}>Новый век технологий</p>
+      </div>
+      <ul className={cn(styles["footer__suggests"], "desk")}>
+        <li className={styles["footer__suggest"]}>
+          <div className={styles["footer__suggest__tick"]}></div>
+          <span>Капельное орошение</span>
+        </li>
+        <li className={styles["footer__suggest"]}>
+          <div className={styles["footer__suggest__tick"]}></div>
+          <span>Упаковка для кормовых культур</span>
+        </li>
+        <li className={styles["footer__suggest"]}>
+          <div className={styles["footer__suggest__tick"]}></div>
+          <span>Упаковка для овощных культур</span>
+        </li>
+      </ul>
+      <div className={cn(styles["contact"], "d-flex", "flex-column")}>
+        <div className={styles["contact__phone__container"]}>
+          <a href="tel:+78005558688" className={styles["contact__phone"]}>8 800 555-86-88</a>
+          <p>Звонок по России - бесплатный</p>
         </div>
-        <ul className={cn(styles["desk-header-contact-list"], "desk")}>
-          <li>
-            <div className={styles["tick"]}></div>Капельное орошение
-          </li>
-          <li>
-            <div className={styles["tick"]}></div>Упаковка для кормовых культур
-          </li>
-          <li>
-            <div className={styles["tick"]}></div>Упаковка для овощных культур
-          </li>
-        </ul>
-        <div className={cn(styles["info"], "d-flex", "flex-column")}>
-          <div className={styles["tel-block"]}>
-            <div className={styles["tel"]}>8 800 555-86-88</div>
-            <p>Звонок по России - бесплатный</p>
-          </div>
-          <div className={styles["site-block"]}>
-            <div className={styles["site"]}>info@neo-agriservis.ru</div>
-            <p>Пн.-Пт.:8:30 - 17:00</p>
-          </div>
-        </div>
-        <div>
-          <p>Мы в соц.сетях</p>
-          <div className={cn("d-flex", "flex-row", "justify-content-between")}>
-            {/**TODO: сделать компонент */}
-            {/**SocialButton src={} alt={} */}
-            <div className={styles["social-btn"]}>
-              <img src={"/icon/inst-icon.png"} alt="Инстаграмм" />
-            </div>
-            <div className={styles["social-btn"]}>
-              <img src={"/icon/vk-icon.png"} alt="Вконтакте" />
-            </div>
-            <div className={styles["social-btn"]}>
-              <img src={"/icon/youtube-icon.png"} alt="Ютуб" />
-            </div>
-          </div>
+        <div className={styles["contact__email__container"]}>
+          <a href="mailto:info@neo-agriservis.ru" className={styles["contact__email"]}>info@neo-agriservis.ru</a>
+          <p>Пн.-Пт.:8:30 - 17:00</p>
         </div>
       </div>
-      {/**About */}
-      <div
-        className={cn(
-          styles["about"],
-          "d-flex",
-          "flex-row",
-          "justify-content-between"
-        )}
-      >
-        <p>
-          © 1998-2019 Все права защищены владельцами торговой марки <br /> ЗАО
-          "Новый век агротехнологий"
-        </p>
-        <div>
-          <a href="#">Положение о защите персональных данных</a>
-          <p>Разработка сайта - drupal-coder.ru</p>
+      <div className={cn(styles["social__content"], "d-flex", "flex-column", "justify-content-center")}>
+        <p className={styles["social__paragraph"]}>Мы в соц.сетях</p>
+        <div className={cn(styles["social__buttons"], "d-flex", "flex-row", "justify-content-between")}>
+          {/**TODO: сделать компонент */}
+          {/**SocialButton src={} alt={} */}
+          <button type="button" className={styles["social__button"]}>
+            <img src={"/icon/inst-icon.png"} alt="Инстаграмм" />
+          </button>
+          <button type="button" className={styles["social__button"]}>
+            <img src={"/icon/vk-icon.png"} alt="Вконтакте" />
+          </button>
+          <button type="button" className={styles["social__button"]}>
+            <img src={"/icon/youtube-icon.png"} alt="Ютуб" />
+          </button>
         </div>
       </div>
-    </>
+    </footer>
   );
 }

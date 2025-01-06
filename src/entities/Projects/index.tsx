69c53c87f96749640fb17bc2effd752476027a21
Veronica <no.demchenko@yandex.ru>
2025-01-06 10:28:53 +0300

    fix(Projects): fix styles for title

--- src/entities/Projects/index.tsx
+++ src/entities/Projects/index.tsx
@@ -5,10 +5,10 @@ import { ProjectsProps } from './Projects.props';
 import styles from './index.module.css';
 export default function Projects({ children } : ProjectsProps) {
     return (
-        <div className={cn(styles["projects"], "restrict-width")}>
-          <div className={styles["title-container"]}>
+        <div className={cn(styles["projects"], "content")}>
+          <div className={cn(styles["title-container"])}>
             <Title level={2} className={styles["title"]}>Реализованные проекты систем капельного орошения</Title>
-            <IconedButton linkUrl={'#'} iconUrl='/icon/flower-icon.png'>Все проекты</IconedButton>
+            <IconedButton className={styles["title__btn"]} linkUrl={'#'} iconUrl='/icon/flower-icon.png'>Все проекты</IconedButton>
           </div>
           
           <div className={cn(styles["projects-container"], "d-flex", "order-1", "order-md-2")}>

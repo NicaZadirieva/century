eb3d47565a6e41106e9ae12edf37ac3f7628d6d0
Veronica <no.demchenko@yandex.ru>
2025-01-06 10:33:23 +0300

    fix(BelievedUsCompanies): fix styles for title

--- src/entities/BelievedUsCompanies/index.tsx
+++ src/entities/BelievedUsCompanies/index.tsx
@@ -6,14 +6,14 @@ export default function BelievedUsCompanies(props: BelievedUsCompaniesProps) {
     const companies = props.images;
     return (
         <>
-        <div className={cn(styles["companies"], "d-flex", "flex-column")}>
-          <div className={styles["title-container"]}>
+        <div className={cn(styles["companies"], "d-flex", "flex-column", "content")}>
+          <div className={cn(styles["title-container"])}>
             <Title level={2}>Нам доверяют</Title>
-            <IconedButton linkUrl={'#'} iconUrl="/icon/message-icon.png">Все клиенты</IconedButton>
+            <IconedButton className={styles["title__btn"]} linkUrl={'#'} iconUrl="/icon/message-icon.png">Все клиенты</IconedButton>
           </div>
           </div>
 
-        <div className={cn(styles["companies-block-img"], "d-flex", "flex-row", "flex-wrap", "justify-content-around")}>
+        <div className={cn(styles["companies-block-img"], "content", "d-flex", "flex-row", "flex-wrap", "justify-content-around")}>
           {
             companies.map((company, index) => (
                 <img alt={company.alt} key={index} src={company.url} width={company.width} height={company.height} />

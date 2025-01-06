9ca47f6f7345773de80740cc3b9a98550a304dd0
Veronica <no.demchenko@yandex.ru>
2025-01-06 22:04:54 +0300

    feat(reviews): add mobile and tablet version
    slider now has inly one slide (max-width: 1200px)

--- src/entities/Reviews/index.tsx
+++ src/entities/Reviews/index.tsx
@@ -1,34 +1,50 @@
-import cn from 'classnames';
-import Slider, { Settings } from 'react-slick';
-import { IconedButton, Title } from '../../widgets';
-import { ReviewsProps } from './Reviews.props';
-import styles from './index.module.css';
-
-
+import cn from "classnames";
+import Slider, { Settings } from "react-slick";
+import { IconedButton, Title } from "../../widgets";
+import { ReviewsProps } from "./Reviews.props";
+import styles from "./index.module.css";
 
 export default function Reviews({ children }: ReviewsProps) {
-  const settings : Readonly<Settings> = {
+  const settings: Readonly<Settings> = {
     centerMode: true,
     dots: true,
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
+    adaptiveHeight: true,
+    lazyLoad: "ondemand",
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+          arrows: true,
+          dots: false,
+          initialSlide: 1
+        },
+      },
+    ],
   };
-    return (
-      <div className={styles["reviews"]}>
-        <div className={cn("restrict-width", "d-flex", "flex-column")}>
-          <div className={cn(styles["title-container"], "d-flex", "flex-row")}>
-            <Title level={2}>Отзывы наших клиентов</Title>
-          
-              <IconedButton linkUrl={'#'} className={styles["button"]} iconUrl="/icon/message-icon.png">
-                Все отзывы
-              </IconedButton>
-          </div>
+  return (
+    <div className={styles["reviews"]}>
+      <div className={cn("restrict-width", "d-flex", "flex-column")}>
+        <div className={cn(styles["title-container"], "d-flex", "content")}>
+          <Title level={2}>Отзывы наших клиентов</Title>
 
-          <div className={cn(styles["reviews-block"])}>
-            <Slider {...settings}>{children}</Slider>
-          </div>
+          <IconedButton
+            linkUrl={"#"}
+            className={styles["button"]}
+            iconUrl="/icon/message-icon.png"
+          >
+            Все отзывы
+          </IconedButton>
+        </div>
+
+        <div className={cn(styles["reviews-block"], "content")}>
+          <Slider {...settings}>{children}</Slider>
         </div>
       </div>
-    );
+    </div>
+  );
 }
